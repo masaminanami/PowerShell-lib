7785dac4c246cc9214d18cc479b9484a738259dc
masaminanami <namami773@local>
2023-02-05 11:13:38 +0900

    Added GetValue2() for migartion support

--- Utils.psm1
+++ Utils.psm1
@@ -3,12 +3,23 @@
 #
 
 class Utils {
-    static [object] GetValue($ht, $key) {
+    static [object] GetValue($ht, [string]$key) {
         if ($ht.Contains($key)) { return $ht.$key }
         throw "ERROR! No data for key: $key"
     }
 
-    static [object] GetValue($ht, $key, $def) {
+    static [object] GetValue($ht, [string]$key, $def) {
         return $ht.Contains($key) ? $ht.$key : $def
+    }
+
+    #--- for format migration
+    static [object] GetValue2($ht, [string]$key, [string]$key2) {
+        if ($ht.Contains($key)) { return $ht.$key }
+        return [Utils]::GetValue($ht, $key2)
+    }
+
+    static [object] GetValue2($ht, [string]$key, [string]$key2, $def) {
+        if ($ht.Contains($key)) { return $ht.$key }
+        return $ht.Contains($key2) ? $ht.$key2 : $def
     }
 }
