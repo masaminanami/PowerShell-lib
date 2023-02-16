13693bae34315b14692edb2ffdedd87cb35c59b0
masaminanami <namami773@local>
2023-02-17 00:29:51 +0900

    WPF View added

---
+++ WPFView.psm1
@@ -0,0 +1,39 @@
+#
+# WPF Window
+#
+
+Add-Type -AssemblyName PresentationCore,PresentationFramework
+
+function New-WindowObject($r) { [System.Windows.Markup.XamlReader]::Load($reader) }
+
+class WPFView {
+    $window;
+
+
+    BaseInit($file) {
+        $this.CreateWindow($file)
+        $this.SetupControls()
+    }
+
+    CreateWindow($file) {
+        log "1 $file"
+
+        [XML]$xaml = Get-Content $file
+        log "2"
+        $this.EditXamlData($xaml)
+        log "3"
+        $reader = [System.Xml.XmlNodeReader]::New($xaml)
+        $this.window = New-WindowObject $reader
+    }
+
+
+    EditXamlData($xaml) {
+        "x:Class","mc:Ignorable" |%{ $xaml.window.RemoveAttribute($_) }
+    }
+
+    #
+    # subclass is expected to implement
+    #
+    SetupControls() {}
+    Init($file) { $this.BaseInit($file) }
+}
