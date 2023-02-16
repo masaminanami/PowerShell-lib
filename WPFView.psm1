bba7ad3e2081d7e8f6fb5145f3898f0587aed0f4
masaminanami <namami773@local>
2023-02-17 00:35:32 +0900

    debug logging removed

--- WPFView.psm1
+++ WPFView.psm1
@@ -9,23 +9,17 @@ function New-WindowObject($r) { [System.Windows.Markup.XamlReader]::Load($r) }
 class WPFView {
     $window;
 
-
     BaseInit($file) {
         $this.CreateWindow($file)
         $this.SetupControls()
     }
 
     CreateWindow($file) {
-        log "1 $file"
-
         [XML]$xaml = Get-Content $file
-        log "2"
         $this.EditXamlData($xaml)
-        log "3"
         $reader = [System.Xml.XmlNodeReader]::New($xaml)
         $this.window = New-WindowObject $reader
     }
-
 
     EditXamlData($xaml) {
         "x:Class","mc:Ignorable" |%{ $xaml.window.RemoveAttribute($_) }
