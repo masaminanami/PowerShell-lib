8f38995f8f38ab0ac3e445c716c19c63c5988c7e
masaminanami <masaminanami@local>
2024-01-08 10:14:37 +0900

    WPFVIew changed

--- WPFView.psm1
+++ WPFView.psm1
@@ -6,10 +6,12 @@ Add-Type -AssemblyName PresentationCore,PresentationFramework
 
 function New-WindowObject($r) { [System.Windows.Markup.XamlReader]::Load($r) }
 
+$script:WPFView;
 class WPFView {
     $window;
 
     BaseInit($file) {
+        $script:WPFView = $this;
         $this.CreateWindow($file)
         $this.SetupControls()
     }
@@ -25,9 +27,79 @@ class WPFView {
         "x:Class","mc:Ignorable" |%{ $xaml.window.RemoveAttribute($_) }
     }
 
+    [Object] FindByName($n) { return $this.window.FindName($n) }
+
+    Show() {
+        $this.window.ShowDialog()
+    }
+
+    CloseWindow() {
+        $this.window.Close()
+    }
+
     #
     # subclass is expected to implement
     #
     SetupControls() {}
     Init($file) { $this.BaseInit($file) }
+}
+
+class WPFAutoResizableView : WPFView {
+    [ResizeSpec]$ResizeSpec;
+    [float]$FixedWidth;
+
+    EnableAutoResize([ResizeSpec]$spec) {
+        $this.ResizeSpec = $spec
+        $this.FixedWidth = $spec.MarginWidth
+        foreach ($e in $this.ResizeSpec.Specs) {
+            if (-not ($e.TargetColumn = $this.window.FindName($e.Name))) {
+                throw "ERROR! $($this.GetType().Name).EnableAutoResize: Cannot find column name: $($e.Name)"
+            }
+            if ($e.IsFixed) {
+                $this.FixedWidth += $e.Width
+            }
+        }
+
+        $this.window.Add_SizeChanged({ $script:WPFView.ResizeColumns()} )
+    }
+
+    ResizeColumns() {
+        $ww = [Math]::Max($this.window.Width, $this.window.ActualWidth) - $this.FixedWidth
+        foreach ($e in $this.ResizeSpec.Specs) {
+            $w = $e.IsFixed ? $e.Width : $e.Width * $ww
+            $e.TargetColumn.Width = [Math]::Max($w, $e.MinWidth)
+        }
+    }
+}
+
+class ResizeSpec {
+    [float]$MarginWidth;
+    [ColumnResizeSpec[]]$Specs;
+
+    ResizeSpec($m, $s) {
+        $this.MarginWidth = $m
+        $this.Specs = $s
+    }
+}
+
+class ColumnResizeSpec {
+    [string]$Name;
+    $TargetColumn;
+    [bool]$IsFixed;
+    [float]$Width;
+    [float]$MinWidth;
+
+    ColumnResizeSpec($n, $f, $w) {
+        $this.Name = $n
+        $this.IsFixed = $f
+        $this.Width = $w
+        $this.MinWidth = $f ? $w : 0
+    }
+
+    ColumnResizeSpec($n, $f, $w, $min) {
+        $this.Name = $n
+        $this.IsFixed = $f
+        $this.Width = $w
+        $this.MinWidth = $min
+    }
 }
